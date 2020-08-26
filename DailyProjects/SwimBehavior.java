fd9e54e5cb4417a237e5377f5e2c61ddf5af3b5e
Kowshik407 <aitha.kowshik3@gmail.com>
2020-08-26 11:31:18 +0530

    First Commit

--- DailyProjects/SwimBehavior.java
+++ DailyProjects/SwimBehavior.java
@@ -1,6 +1,6 @@
 public class SwimBehavior extends Bird {
     
     public void swim() {
-        System.out.println("The auatic animals can swim");
+        System.out.println("The aquatic animals can swim");
     }
 }
