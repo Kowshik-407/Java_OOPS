100af211ba5826f005ee19ad335f1942007091be
Kowshik407 <aitha.kowshik3@gmail.com>
2020-08-26 19:34:13 +0530

    Changed

--- DailyProjects/Bird.java
+++ DailyProjects/Bird.java
@@ -9,6 +9,27 @@ abstract class Bird {
      * Penguin class will extend Bird and implements SwimBehaviour
      */
 
+     /**
+      * 
+      * Extend the assignment 7 and add a new method in the Bird class with a name eat()
+      * override the method eat() in the classes Ostrich, Parrot, Penguin 
+      * Add the overloaded of method of eat in the classes Ostrich, Parrot, Penguin
+      * Overloaded will look like public void eat(String item) {    }
+      */
+
+      /**
+       * 
+       * Create a class constructBirds and create the objects of classes Ostrich, Parrot, Penguin with 
+       * reference variables as Bird. 
+       * 
+       * Eg. Bird birdObj1 = new Ostrich()
+       * Call both the eat methods on the object
+       * birdObj1.eat();
+       * birdObj1.eat("insect");
+       * 
+       * @param args
+       */
+
     public static void main(String[] args) {
         
         FlyBehavior birdobj = new FlyBehavior();
